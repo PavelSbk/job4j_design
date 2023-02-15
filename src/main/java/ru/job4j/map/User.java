d3e7bc7ba3ff78ce0e8c55679ca581ad52c5b0bc
Pavel Shebeko <shebeko_pavel@mail.ru>
2023-02-15 15:03:20 +0300

    5. Переопределить только hashCode [#1003 #391914]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -18,6 +18,11 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
     public int hashCode() {
         return Objects.hash(name, children, birthday);
     }
