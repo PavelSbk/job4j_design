e4e708b423514a94da0550ff6f380a54e45c7c7b
Pavel Shebeko <shebeko_pavel@mail.ru>
2023-02-19 18:28:21 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+
+    public static int[][] multiple(int size) {
+        int[][] array = new int[size][size];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return array;
+    }
+
+    public static String tableToString(int[][] array) {
+        var stringB = new StringBuilder();
+        for (int[] arr : array) {
+            for (int i : arr) {
+                stringB.append(i).append(" ");
+            }
+            stringB.append(System.lineSeparator());
+        }
+        return stringB.toString();
+    }
+
+    public static void writeMTable(int size, String path) {
+        try (FileOutputStream out = new FileOutputStream(path)) {
+            out.write(tableToString(multiple(size)).getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            out.write("Hello, world!".getBytes());
+            out.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        writeMTable(20, "data/multipleTable.txt");
+    }
+}
