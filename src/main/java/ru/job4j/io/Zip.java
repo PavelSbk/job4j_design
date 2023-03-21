a0d4c1c72a123c6b14f449af3404d41a23595546
Pavel Shebeko <shebeko_pavel@mail.ru>
2023-03-21 09:20:58 +0300

    14. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,69 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipOutputStream;
+import java.io.*;
+import java.util.zip.ZipEntry;
+
+public class Zip {
+
+    public void packFiles(List<File> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File f : sources) {
+                zip.putNextEntry(new ZipEntry(f.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(f))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void validator(ArgsName value) {
+        if (!Files.isDirectory(Paths.get(value.get("d")))) {
+            throw new IllegalArgumentException("Root folder setting error.");
+        }
+        if (!value.get("e").startsWith(".") || value.get("e").length() < 2) {
+            throw new IllegalArgumentException("Wrong file extension.");
+        }
+        if (!value.get("o").endsWith(".zip")) {
+            throw new IllegalArgumentException("Wrong file extension '.zip' missed");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Root folder has a value not equal to three. Usage ROOT_FOLDER.");
+        }
+        ArgsName arg = ArgsName.of(args);
+        validator(arg);
+        Path source = Paths.get(arg.get("d"));
+        List<File> list = Search
+                .search(source, path -> !path.toFile().getName().endsWith(arg.get("e")))
+                .stream()
+                .map(Path::toFile)
+                .toList();
+        Zip zip = new Zip();
+        zip.packSingleFile(
+                new File("./pom.xml"),
+                new File("./pom.zip")
+        );
+        zip.packFiles(list, new File(arg.get("o")));
+    }
+}
