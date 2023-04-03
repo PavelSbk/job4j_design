d0cc0df793994e571414dc8102e4f186db61973d
Pavel Shebeko <shebeko_pavel@mail.ru>
2023-04-03 09:35:15 +0300

    0. Что такое база данных. [#6860 #405809]

--- db/books_script.sql
+++ db/books_script.sql
@@ -1,4 +1,4 @@
-create TABLE books(
+create TABLE books (
 	book_id serial PRIMARY KEY, 
     title text,
     pages integer,
@@ -12,9 +12,18 @@ values
 (2,	'Effective Java', 412, 'Joshua Bloch', 0134685997),
 (3,	'Grokking Algorithms', 300, 'Aditya Bhargava', 1617292230),
 (4,	'Head First Java',	688, 'bof',	1491910771),
+
+insert into books
+values
 (5,	'Java Concurrency in Practice',	424, 'Brian Goetz',	0321349601);
 
-select * from books;
+UPDATE books
+SET isbn = 666666666
+WHERE book_id = 5
+
+delete from books where book_id = 5
+
+select * from books order by book_id;
 
 
 
